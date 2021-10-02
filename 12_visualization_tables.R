3d24a1cc861cf06c0376dba6a6fda7b6f42bfdf5
Zoe <59600387+zoeschindler@users.noreply.github.com>
2021-10-02 16:52:51 +0200

    Update 12_visualization_tables.R

--- 12_visualization_tables.R
+++ 12_visualization_tables.R
@@ -988,7 +988,7 @@ ggplot(
   stat_boxplot(geom = "errorbar", alpha = 1, width = 0.2, position = position_dodge(0.9)) +
   geom_boxplot(outlier.alpha = 1, outlier.size = 1, position = position_dodge(0.9)) +
   scale_fill_manual(
-    values = color_scale_class,
+    values = color_scale_type,
     name = "Input Data\nCombination",
     labels = c("TLS", "TLS & GEO", "TLS & RGB", "ALL")
   ) +
